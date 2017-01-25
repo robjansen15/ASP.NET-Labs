ff8eb46afa97426d99f1fd0fe0766999ae81460a
Robert Jansen <robjansen15@outlook.com>
2017-01-25 11:04:52 -0500

    init commit

--- ASP.NET/Labs/Lab2/Default.aspx
+++ ASP.NET/Labs/Lab2/Default.aspx
@@ -1,15 +1,26 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Lab2.Default" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Lab2.Default"%>
 
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <title>Lab2</title>
 </head>
 <body>
     <form id="form1" runat="server">
-        <div>
+        
+        <div id="mainDiv">
+            <h1>Lab 2</h1>
+            
+            <asp:TextBox runat="server" id="textBox">HI</asp:TextBox>
+            
+            <asp:DropDownList ID="myList" runat="server" EnableViewState="True" OnSelectedIndexChanged="myList_OnSelectedIndexChanged">           
+                <asp:ListItem Value="dummy" selected="True"></asp:ListItem>    
+            </asp:DropDownList>
+            
+
         </div>
+
     </form>
 </body>
 </html>
--- ASP.NET/Labs/Lab2/Default.aspx.cs
+++ ASP.NET/Labs/Lab2/Default.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +12,28 @@ namespace Lab2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Page.IsPostBack)
+            {
+                //textBox.Style["font-size"] = "200px";
+                //textBox.BackColor = Color.Red;
 
+                for (int i = 0; i < 3; i++)
+                {
+                    ListItem li = new ListItem("abc", "abc");
+                    myList.Items.Add((li));
+                }
+            }
+        }
+
+
+        protected void myList_OnSelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Response.Write("<script>alert('" + "Your information has been submitted: " +
+            // myList.SelectedValue +
+            // "');</script>");
+
+            var x = 10;
         }
     }
+
 }
