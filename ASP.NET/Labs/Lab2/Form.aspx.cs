eb9a0187f5d8aa716b6574bddf5280874852ee03
Rob <robjansen15@gmail.com>
2017-01-27 18:02:03 -0500

    init lab2

---
+++ ASP.NET/Labs/Lab2/Form.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab1
+{
+    public partial class Form : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //scrape all data
+            var firstName = this.firstName.Text;
+            var lastName = this.lastName.Text;
+            var email = this.email.Text;
+            var emailCheck = this.emailConfirm.Text;
+            var password = this.passwordInput.Text;
+            var passwordCheck = this.passwordInputCheck.Text;
+            var gender = this.personGender.SelectedValue;
+            var department = this.department.SelectedValue;
+            var status = this.personStatus.SelectedValue;      
+            var agreementIsChecked = this.agreeToTerms.Checked;
+
+        
+            //output data
+            Response.Write("<script>alert('"+ "Your information has been submitted: " + 
+                firstName + "  " +
+                lastName + "  " +
+                email + "  " +
+                emailCheck + "  " +
+                password + "  " +
+                passwordCheck + "  " +
+                gender + "  " +
+                department + "  " +
+                status + "  " +
+                agreementIsChecked + "  " +
+                "');</script>");
+        }
+    }
+}
--- ASP.NET/Labs/Lab2/Properties/AssemblyInfo.cs
+++ ASP.NET/Labs/Lab2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lab2")]
+[assembly: AssemblyTitle("Lab1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lab2")]
+[assembly: AssemblyProduct("Lab1")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("17431d3b-1145-47e6-8966-d880d7d42f9b")]
+[assembly: Guid("5fb65fff-d692-436c-ac63-26b4a3231ab7")]
 
 // Version information for an assembly consists of the following four values:
 //
