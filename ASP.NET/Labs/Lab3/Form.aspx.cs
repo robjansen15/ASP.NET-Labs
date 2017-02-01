97f38eaad8a3e5997fcc10a7b13641a85fed004c
Robert Jansen <robjansen15@outlook.com>
2017-02-01 11:28:52 -0500

    lab3 commit

---
+++ ASP.NET/Labs/Lab3/Form.aspx.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Threading;
+
+namespace Lab3
+{
+    public partial class Form : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                string JQueryVer = "1.7.1";
+                ScriptManager.ScriptResourceMapping.AddDefinition("jquery", new ScriptResourceDefinition
+                {
+                    Path = "~/Scripts/jquery-" + JQueryVer + ".min.js",
+                    DebugPath = "~/Scripts/jquery-" + JQueryVer + ".js",
+                    CdnPath = "http://ajax.aspnetcdn.com/ajax/jQuery/jquery-" + JQueryVer + ".min.js",
+                    CdnDebugPath = "http://ajax.aspnetcdn.com/ajax/jQuery/jquery-" + JQueryVer + ".js",
+                    CdnSupportsSecureConnection = true,
+                    LoadSuccessExpression = "window.jQuery"
+                });
+            }
+        }
+
+        protected void Submit_Button(object sender, EventArgs e)
+        {
+            //scrape all data
+            var firstName = this.firstName.Text;
+            var lastName = this.lastName.Text;
+            var email = this.email.Text;
+            var emailCheck = this.emailConfirm.Text;
+            var password = this.passwordInput.Text;
+            var passwordCheck = this.passwordInputCheck.Text;
+            var gender = this.personGender.SelectedValue;
+            var department = this.department.SelectedValue;
+            var status = this.personStatus.SelectedValue;
+            var agreementIsChecked = this.agreeToTerms.Checked;
+
+
+            if (firstName.Length > 0 && lastName.Length > 0)
+            {
+                //output data
+                this.webOutput.Text = "";
+                this.webOutput.Text += ("Please look at the information you submitted and check if this is correct: " +
+                    firstName + "  " +
+                    lastName + "  " +
+                    email + "  " +
+                    emailCheck + "  " +
+                    password + "  " +
+                    passwordCheck + "  " +
+                    gender + "  " +
+                    department + "  " +
+                    status + "  " +
+                    agreementIsChecked + "  ");
+
+                this.submitBtn.Visible = false;
+                this.verifyButton.Visible = true;
+                this.cancelBtn.Visible = true;
+                this.webOutput.Visible = true;
+            }
+            else
+            {
+                string output = "Please enter your ";
+
+                if (firstName.Length < 1)
+                {
+                    this.firstName.BackColor = System.Drawing.Color.Yellow;
+                    output += "first name";
+                }
+
+                if (lastName.Length < 1)
+                {
+                    this.lastName.BackColor = System.Drawing.Color.Yellow;
+                    output += ", last name";
+                }
+
+
+                Response.Write("<script>alert('" + output +
+                   "');</script>");
+            }
+        }
+
+
+        protected void Verify_Click(object sender, EventArgs e)
+        {
+            Response.Write("<script>alert('" + "Thank You for submitting the form!" +
+                   "');</script>");
+
+        }
+
+
+        protected void Cancel_Click(object sender, EventArgs e)
+        {
+            this.submitBtn.Visible = true;
+            this.verifyButton.Visible = false;
+            this.cancelBtn.Visible = false;
+            this.webOutput.Visible = false;
+        }
+    }    
+}
---
+++ ASP.NET/Labs/Lab3/Home.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="Lab3.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta charset="UTF-8">
+	<title>Lab1</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="header">
+		    <div>
+			    <a href="Home.aspx" id="logo"><img src="images/logo.png" alt="logo"></a>
+			    <ul>
+				    <li class="selected">
+					    <a href="Home.aspx">Home</a>
+				    </li>
+				    <li>
+					    <a href="Form.aspx">Form</a>
+				    </li>		   
+			    </ul>
+		    </div>
+	    </div>
+	    <div id="body" class="home">
+		    <h1 style="text-align:center">Welcome to my Form!</h1>
+            
+            <br/>
+            <br/>
+
+            <p>To get started please click on the "form" tab to submit your information.</p>
+            
+            <br/>
+            
+            <p>If you have any questions regarding this webpage feel free to contact me at fake@email.com</p>
+	    </div>
+	    <div id="footer">
+		    <div>
+			    <p>
+				    &#169; Rob Jansen
+			    </p>
+			    <div class="connect">
+				    <a href="http://freewebsitetemplates.com/go/googleplus/" id="googleplus">google+</a> <a href="http://www.freewebsitetemplates.com/misc/contact" id="contact">contact</a> <a href="http://freewebsitetemplates.com/go/facebook/" id="facebook">facebook</a> <a href="http://freewebsitetemplates.com/go/twitter/" id="twitter">twitter</a>
+			    </div>
+		    </div>
+	    </div>
+    </form>
+</body>
+</html>
+
