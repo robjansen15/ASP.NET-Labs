9861eb6cc8a8aa21c118b2d363a934c01e014d72
Robert Jansen <robjansen15@outlook.com>
2017-03-24 22:51:25 -0400

    save

---
+++ ASP.NET/Labs/SQLQuery3.sql
@@ -0,0 +1,11 @@
+﻿Alter PROCEDURE [dbo].[ChangePassword]
+	@USER varchar(50),
+	@NEWPASS varchar(50)
+AS
+	Update Login
+	set PASSWORD = @NEWPASS
+	where EMAIL = @USER
+RETURN 0
+
+select * from Login
+
